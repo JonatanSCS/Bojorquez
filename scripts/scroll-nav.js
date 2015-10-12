31499967d7daaae4db0a4f774ca032f5c9e43991
JonatanSCS <jonatanscs97@hotmail.com>
2015-10-12 17:50:45 -0500

    Agregando Navegador estático

---
+++ scripts/scroll-nav.js
@@ -0,0 +1,29 @@
+
+
+$(window).scroll(function(){
+	if($(document).scrollTop() > 100) {
+      $(".header-banner").css("display", "none");
+      $(".header-nav").css("position", "fixed");
+      $(".header-nav").css("z-index", "1000");
+      $(".header-nav").css("margin-left", "-30%");
+      $(".header-nav").css("margin-top", "-30px");
+      $(".header-nav").css("opacity", ".9");
+      $(".estados-section-fixed").css("margin-top", "-40px");
+      $(".arte-cultura-section-fixed").css("margin-top", "-40px");
+
+
+  }
+ });
+$(window).scroll(function(){
+	if($(document).scrollTop() < 100) {
+      $(".header-banner").css("display", "block");
+      $(".header-nav").css("position", "relative");
+      $(".header-nav").css("margin-left", "0");
+      $(".header-nav").css("margin-top", "0");
+      $(".header-nav").css("opacity", "1");
+      $(".estados-section-fixed").css("margin-top", "-240px");
+      $(".arte-cultura-section-fixed").css("margin-top", "-240px");
+
+
+  }
+ });
