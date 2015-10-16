5bd754e41c52877923d6a549c9f378384dbca841
JonatanSCS <jonatanscs97@hotmail.com>
2015-10-15 20:42:43 -0500

    Fusion de version Jonatan y Damian

---
+++ scripts/jquery-changes.js
@@ -0,0 +1,30 @@
+$(window).scroll(function(){
+	if($(document).scrollTop() > 210) {
+    	$(".header-top").css("display", "none");
+    	$("header").css("position", "fixed");
+    	$("header").css("width","100%");
+    	$("header").css("margin-top", "-30px");
+    	$("nav").css("margin-top","0");
+    	$(".estados-section").css("position", "fixed");
+    	$(".arte-section").css("position", "fixed");
+    	$(".estados-section").css("margin-top", "45px");
+    	$(".arte-section").css("margin-top", "45px");
+  }
+ });
+$(window).scroll(function(){
+	if($(document).scrollTop() < 210) {
+		$(".header-top").css("display", "block");
+   		$("header").css("position", "relative");
+    	$("header").css("width","100%");
+    	$("nav").css("margin-top","1%");
+    	$(".logo-section").css("margin-top", "20px");
+    	$(".estados-section").css("position", "absolute");
+    	$(".arte-section").css("position", "absolute");
+    	$(".estados-section").css("margin-top", "15px");
+    	$(".arte-section").css("margin-top", "15px");
+    	
+
+  }
+ });
+
+
