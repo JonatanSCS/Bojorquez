e545c98b4a46721c28f0cb115aed8b27abc10b16
JonatanSCS <jonatanscs97@hotmail.com>
2015-11-13 23:58:55 -0600

    Cambiando Header por propuesta de Jorge Garza

--- scripts/jquery-changes.js
+++ scripts/jquery-changes.js
@@ -1,37 +1,40 @@
 $(window).scroll(function(){
-	if($(document).scrollTop() > 163) {
-    	$(".header-top").css("display", "none");
-        $(".viajes-bojorquez-link").css("display", "none");
-    	$("header").css("position", "fixed");
-    	$("header").css("margin-top", "-30px");
-    	$(".estados-section").css("position", "fixed");
-    	$(".arte-section").css("position", "fixed");
-    	$(".estados-section").css("margin-top", "33px");
-    	$(".arte-section").css("margin-top", "33px");
-        $(".principal-image").css("margin-top", "227px");
-        $(".imagen-estado").css("margin-top", "227px");
-        $(".municipio-info-tabla").css("margin-top", "227px")
-
-        
-
+	if($(document).scrollTop() > 100) {
+        $('header').css('position', 'fixed');
+        $('header').css('margin-top', '-30px');
+        $('.banner').css('display', 'none');
+    	$('.guia-turistica-logo').css('display', 'none');
+        $('.viajes-logo').css('display', 'none');
+        $('.principal-image').css('margin-top', '145px');
+        $('.nav-ul').css('margin-top', '-3px');
+        $('.arte-section').css('margin-top', '19px');
+        $('.estados-section').css('margin-top', '19px');
+        $('.arte-section').css('position', 'fixed');
+        $('.estados-section').css('position', 'fixed');
+        $('.imagen-estado').css('margin-top', '145px');
+        $('.municipio-info-tabla').css('margin-top', '145px');
   }
  });
 $(window).scroll(function(){
-	if($(document).scrollTop() < 163) {
-        $(".header-top").css("display", "block");
-        $(".viajes-bojorquez-link").css("display", "block");
-        $("header").css("position", "relative");
-        $("header").css("margin-top", "0");
-        $(".estados-section").css("position", "absolute");
-        $(".arte-section").css("position", "absolute");
-        $(".estados-section").css("margin-top", "5px");
-        $(".arte-section").css("margin-top", "5px");
-        $(".principal-image").css("margin-top", "0px");
-        $(".imagen-estado").css("margin-top", "0px");
-        $(".municipio-info-tabla").css("margin-top", "0px")
+	if($(document).scrollTop() < 100) {
+        $('header').css('position', 'relative');
+        $('header').css('margin-top', '0');
+        $('.banner').css('display', 'inline-block');
+        $('.guia-turistica-logo').css('display', 'inline-block');
+        $('.viajes-logo').css('display', 'inline-block');
+        $('.principal-image').css('margin-top', '0');
+        $('.nav-ul').css('margin-top', '3px');
+        $('.arte-section').css('margin-top', '2px');
+        $('.estados-section').css('margin-top', '2px');
+        $('.arte-section').css('position', 'absolute');
+        $('.estados-section').css('position', 'absolute');
+        $('.imagen-estado').css('margin-top', '0');
+        $('.municipio-info-tabla').css('margin-top', '0');
 
   }
  });
+
+
 
 
 
